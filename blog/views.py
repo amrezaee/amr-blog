468e766e743aa18219347b4b31b5231a36c80973
Amirmohamad Rezaee <amrezaee@protonmail.com>
2025-03-25 18:53:10 +0100

    change templates to use models

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post, Author, Tag
 
 def start_page(request):
-    return render(request, "blog/start_page.html")
+    latests_posts = Post.objects.all().order_by("-date")[:2]
+    return render(request, "blog/start_page.html",{
+        "posts": latests_posts
+    })
 
 
 def list_posts(request):
-    return render(request, "blog/all_posts.html")
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all_posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def show_post(request, slug):
-    return render(request, "blog/post_detail.html")
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post_detail.html",{
+        "post": post,
+        "author": post.author
+    })
