b5ab532657ee777abcb479cff211d560ef7e7886
Amirmohamad Rezaee <amrezaee@protonmail.com>
2025-04-27 21:44:12 +0100

    rename project

--- amr_blog/asgi.py
+++ amr_blog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for amr_portfolio_website project.
+ASGI config for amr_blog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amr_portfolio_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amr_blog.settings')
 
 application = get_asgi_application()
--- amr_blog/wsgi.py
+++ amr_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for amr_portfolio_website project.
+WSGI config for amr_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amr_portfolio_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amr_blog.settings')
 
 application = get_wsgi_application()
