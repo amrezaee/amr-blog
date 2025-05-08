1bb27a7dbe5161b18516d784120534adb28b4fb5
Amirmohamad Rezaee <amrezaee@protonmail.com>
2025-05-08 21:30:46 +0100

    change Post model image_name field to ImageField and setup file uploads

--- amr_blog/settings.py
+++ amr_blog/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
