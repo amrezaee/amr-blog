e5c2fc653aa28d4197cc9e3b17b0e495a9cbd0f7
Amirmohamad Rezaee <amrezaee@protonmail.com>
2025-05-08 23:24:43 +0100

    add read-later for posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.StartPageView.as_view(), name="start_page"),
     path("posts/", views.ListPostsView.as_view(), name="list_posts"),
     path("posts/<slug:slug>", views.PostDetailView.as_view(), name="show_post"),
+    path("read-later", views.ReadLaterView.as_view(), name="read_later"),
 ]
